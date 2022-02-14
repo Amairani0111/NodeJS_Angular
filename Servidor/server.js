80090975056c5770ff0394816974922f8adf04e7
Amairani Fernanda Rodriguez Garcia <amairani.rodriguez1995@gmail.com>
2022-02-13 21:48:33 -0600

    Primer commit

---
+++ Servidor/server.js
@@ -0,0 +1,44 @@
+//Define el http
+const { Console } = require('console');
+const http = require('http');
+
+//Arreglo JSON
+const libros = [
+    {'titulo': 'El universo de lo sencillo', 'autor': 'Pablo Arribas'},
+    {'titulo': 'Soñando Despierta', 'autor': 'Carla Medina'},
+    {'titulo': 'Si, si es contigo', 'autor': 'Calle y Poche'},
+]
+
+//Crear servidor
+const server = http.createServer(( request, response) => {
+    //Imprime request que envía el cliente
+    //console.log(request);
+
+    //Imprimir propiedades del request (header, url y metodo)
+    //const { headers, url, method } = request;
+    //console.log(headers, url, method);
+
+    //Texto tipo plano
+    //response.setHeader('Content-Type', 'text/plain')
+
+    //Texto tipo html 
+    //response.setHeader('Content-Type', 'text/html')
+
+    //Imprimir texto
+    //response.write('<h1>Hola mundo</h1>');
+
+    //Texto tipo JSON 
+    response.setHeader('Content-Type', 'application/json')
+
+    //Devuelve el response al cliente
+    response.end(
+        JSON.stringify({data: libros})
+    );
+});
+
+//Puerto en donde el servidor se va a levantar
+const PORT = 5000;
+
+//Levantar el servidor
+server.listen(PORT, () => console.log('El servidor se está ejecutando'));
+
